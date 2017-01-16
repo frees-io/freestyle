07c807594a591937c687194a8d54910d086ea3c0
Javi Pacheco <javi@47deg.com>
2017-01-16 16:51:58 +0100

    Adding vendor to travis

--- scripts/decrypt-keys.sh
+++ scripts/decrypt-keys.sh
@@ -2,5 +2,4 @@
 
 openssl aes-256-cbc -K $encrypted_86276aecec54_key -iv $encrypted_86276aecec54_iv -in secring.gpg.enc -out secring.gpg -d;
 chmod 600 secring.gpg;
-eval `ssh-agent -s`;
-ssh-add secring.gpg;
+cp secring.gpg ~/.ssh/id_rsa;
