9ed8fd46b5ae08f114b4c9106e44006c60296ca4
Javi Pacheco <javi@47deg.com>
2017-01-17 11:26:07 +0100

    Copying key to id_rsa

--- scripts/decrypt-keys.sh
+++ scripts/decrypt-keys.sh
@@ -3,5 +3,4 @@
 openssl aes-256-cbc -K $encrypted_fc716ecc5668_key -iv $encrypted_fc716ecc5668_iv -in keys.tar.enc -out keys.tar -d
 tar xvf keys.tar;
 chmod 600 travis-deploy-key;
-eval "$(ssh-agent -s)"
-ssh-add travis-deploy-key
+cp travis-deploy-key ~/.ssh/id_rsa;
