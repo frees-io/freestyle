e0d8194c470e17f5d7ebcd8c97024d0a89fc202f
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2017-02-09 12:16:49 +0000

    FS-37 Cache Algebra
    We add the simple algebra of Cache Operations, to represent
    key-value stores. We provide a simple implementation, based
    on a java Concurrent Hash Map.

--- freestyle/shared/src/main/scala/freestyle/Capture.scala
+++ freestyle/shared/src/main/scala/freestyle/Capture.scala
@@ -5,6 +5,13 @@ import simulacrum.typeclass
 
 import scala.util.Try
 
+/*
+ * The method `Applicative#pure` in `cats.Applicative` is strict on its parameter. Thus, it
+ *  forces the evaluation of any expression passed to it.
+ *
+ * However, since we need to support different types in the Interpreters, we need to
+ *  define a `Capture` type-class..
+ */
 @typeclass
 trait Capture[F[_]] {
   def capture[A](a: => A): F[A]
