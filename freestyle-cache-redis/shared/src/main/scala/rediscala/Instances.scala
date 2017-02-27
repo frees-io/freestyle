58af82615bd7d70fb7fed9396b7843732fa147c0
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2017-02-27 14:54:01 +0000

    FS-37-Correction Hide Rediscala
    We modify the implementation of the `rediscala` integration, so as to
    not require rediscala's serializers or deserialisers.
    This is to avoid tying users of `freestyle-cache-redis` to a client
    library, `rediscala`, in particular.

--- freestyle-cache-redis/shared/src/main/scala/rediscala/Instances.scala
+++ freestyle-cache-redis/shared/src/main/scala/rediscala/Instances.scala
@@ -8,7 +8,7 @@ import _root_.redis.commands.Transactions
  * parallel fragments execute their operations as _parallel_ as possible.
  * Since Redis is a single-threaded server, _parallel_ means sending operations together
  * in a single batch, which is possible if there are no data dependencies between them. */
-class Interpret[F[_]](client: Transactions) extends (Ops[F, ?] ~> F) {
+class Interpret[F[_]](client: Transactions) extends (Kleisli[F, Commands, ?] ~> F) {
 
   override def apply[A](fa: Kleisli[F, Commands, A]): F[A] = {
     val transaction = client.transaction()
--- freestyle-cache-redis/shared/src/main/scala/rediscala/RedisMap.scala
+++ freestyle-cache-redis/shared/src/main/scala/rediscala/RedisMap.scala
@@ -3,17 +3,22 @@ package freestyle.cache.redis.rediscala
 import cats.{~>, Functor}
 import cats.syntax.functor._
 import scala.concurrent.Future
-import _root_.redis.{ByteStringSerializer => Serializer, ByteStringDeserializer => Deserializer}
+import _root_.redis.{ByteStringDeserializer, ByteStringSerializer}
 import freestyle.cache.KeyValueMap
 
 class MapWrapper[M[_], Key, Value](
-    implicit format: Format[Key],
-    parser: Parser[Key],
-    read: Deserializer[Option[Value]],
-    writer: Serializer[Value],
+    implicit formatKey: Format[Key],
+    parseKey: Parser[Key],
+    formatVal: Format[Value],
+    parseVal: Parser[Value],
     toM: Future ~> M,
     funcM: Functor[M]
 ) extends KeyValueMap[Ops[M, ?], Key, Value] {
+
+  private[this] implicit val serial: ByteStringDeserializer[Option[Value]] =
+    ByteStringDeserializer.String.map(parseVal)
+  private[this] implicit val deserial: ByteStringSerializer[Value] =
+    ByteStringSerializer.String.contramap(formatVal)
 
   override def get(key: Key): Ops[M, Option[Value]] =
     RediscalaCont.get[Key, Option[Value]](key).transform(toM).map(_.flatten)
@@ -28,7 +33,7 @@ class MapWrapper[M[_], Key, Value](
     RediscalaCont.exists(key).transform(toM)
 
   override def keys: Ops[M, List[Key]] =
-    RediscalaCont.keys.transform(toM).map(_.toList.flatMap(parser.apply))
+    RediscalaCont.keys.transform(toM).map(_.toList.flatMap(parseKey.apply))
 
   override def clear(): Ops[M, Unit] =
     RediscalaCont.flushDB.transform(toM).void
@@ -38,10 +43,10 @@ class MapWrapper[M[_], Key, Value](
 object MapWrapper {
 
   implicit def apply[M[_], Key, Value](
-      implicit format: Format[Key],
-      parser: Parser[Key],
-      read: Deserializer[Option[Value]],
-      writer: Serializer[Value],
+      implicit formatKey: Format[Key],
+      parseKey: Parser[Key],
+      formatValue: Format[Value],
+      parseValue: Parser[Value],
       toM: Future ~> M,
       funcM: Functor[M]
   ): MapWrapper[M, Key, Value] = new MapWrapper
