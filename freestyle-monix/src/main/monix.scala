8f909775d122755c075e2f2b2abbe519ffb380c9
peterneyens <peter.neyens@gmail.com>
2017-03-15 11:24:42 +0000

    Fix monix Task Capture instance

--- freestyle-monix/src/main/monix.scala
+++ freestyle-monix/src/main/monix.scala
@@ -3,7 +3,7 @@ package freestyle
 import monix.eval.Task
 
 object monix {
-  implicit val freestyleMonixCaptureInstance: Capture[Task] {
+  implicit val freestyleMonixCaptureInstance: Capture[Task] = Capture[Task] {
     override def capture[A](a: => A) = Task(a)
   }
 }
