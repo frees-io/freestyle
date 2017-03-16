4ae55c59e50fd82d4bfac4bf964c4dc42a614246
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2017-03-16 20:13:54 +0000

    FS-120 Split Tests Module
    It turns out that most of the tests in the `tests` module only depend
    on freestyle, and a few test on monix. We move tests to `freestyle`,
    and we add monix as a test-only dependency.

---
+++ freestyle/shared/src/test/scala/freestyle/Utils.scala
@@ -0,0 +1,137 @@
+package freestyle
+
+import cats.arrow.FunctionK
+
+object algebras {
+
+  @free
+  trait SCtors1[F[_]] {
+    def x(a: Int): FreeS[F, Int]
+    def y(a: Int): FreeS[F, Int]
+  }
+
+  @free
+  trait SCtors2[F[_]] {
+    def i(a: Int): FreeS[F, Int]
+    def j(a: Int): FreeS[F, Int]
+  }
+
+  @free
+  trait SCtors3[F[_]] {
+    def o(a: Int): FreeS[F, Int]
+    def p(a: Int): FreeS[F, Int]
+  }
+
+  @free
+  trait SCtors4[F[_]] {
+    def k(a: Int): FreeS[F, Int]
+    def m(a: Int): FreeS[F, Int]
+  }
+
+  @free
+  trait MixedFreeS[F[_]] {
+    def x: FreeS.Par[F, Int]
+    def y: FreeS.Par[F, Int]
+    def z: FreeS[F, Int]
+  }
+
+  @free
+  trait S1[F[_]] {
+    def x(n: Int): FreeS[F, Int]
+  }
+
+  @free
+  trait S2[F[_]] {
+    def y(n: Int): FreeS[F, Int]
+  }
+
+}
+
+object modules {
+
+  import algebras._
+
+  @module
+  trait M1[F[_]] {
+    val sctors1: SCtors1[F]
+    val sctors2: SCtors2[F]
+  }
+
+  @module
+  trait M2[F[_]] {
+    val sctors3: SCtors3[F]
+    val sctors4: SCtors4[F]
+  }
+
+  @module
+  trait O1[F[_]] {
+    val m1: M1[F]
+    val m2: M2[F]
+  }
+
+  @module
+  trait O2[F[_]] {
+    val o1: O1[F]
+    val x = 1
+    def y = 2
+  }
+
+  @module
+  trait O3[F[_]] {
+    def x = 1
+    def y = 2
+  }
+
+  @module
+  trait StateProp[F[_]] {
+    val s1: S1[F]
+    val s2: S2[F]
+  }
+
+}
+
+object interps {
+
+  import algebras._
+
+  implicit val optionHandler1: FunctionK[SCtors1.Op, Option] = new SCtors1.Handler[Option] {
+    def x(a: Int): Option[Int] = Some(a)
+    def y(a: Int): Option[Int] = Some(a)
+  }
+
+  implicit val listHandler1: FunctionK[SCtors1.Op, List] = new SCtors1.Handler[List] {
+    def x(a: Int): List[Int] = List(a)
+    def y(a: Int): List[Int] = List(a)
+  }
+
+  implicit val optionHandler2: FunctionK[SCtors2.Op, Option] = new SCtors2.Handler[Option] {
+    def i(a: Int): Option[Int] = Some(a)
+    def j(a: Int): Option[Int] = Some(a)
+  }
+
+  implicit val listHandler2: FunctionK[SCtors2.Op, List] = new SCtors2.Handler[List] {
+    def i(a: Int): List[Int] = List(a)
+    def j(a: Int): List[Int] = List(a)
+  }
+
+  implicit val optionHandler3: FunctionK[SCtors3.Op, Option] = new SCtors3.Handler[Option] {
+    def o(a: Int): Option[Int] = Some(a)
+    def p(a: Int): Option[Int] = Some(a)
+  }
+
+  implicit val listHandler3: FunctionK[SCtors3.Op, List] = new SCtors3.Handler[List] {
+    def o(a: Int): List[Int] = List(a)
+    def p(a: Int): List[Int] = List(a)
+  }
+
+  implicit val optionHandler4: FunctionK[SCtors4.Op, Option] = new SCtors4.Handler[Option] {
+    def k(a: Int): Option[Int] = Some(a)
+    def m(a: Int): Option[Int] = Some(a)
+  }
+
+  implicit val listHandler4: FunctionK[SCtors4.Op, List] = new SCtors4.Handler[List] {
+    def k(a: Int): List[Int] = List(a)
+    def m(a: Int): List[Int] = List(a)
+  }
+
+}
---
+++ freestyle/shared/src/test/scala/freestyle/parallel.scala
@@ -0,0 +1,136 @@
+package freestyle
+
+import cats.data.Kleisli
+import cats.implicits._
+import monix.eval.Task
+import org.scalatest.{Matchers, WordSpec}
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.duration.Duration
+
+class parallelTests extends WordSpec with Matchers {
+
+  "Applicative Parallel Support" should {
+
+    import algebras._
+
+    class NonDeterminismTestShared {
+      import freestyle.nondeterminism._
+      import freestyle.implicits._
+
+      val buf = scala.collection.mutable.ArrayBuffer.empty[Int]
+
+      def blocker(value: Int, waitTime: Long): Int = {
+        Thread.sleep(waitTime)
+        buf += value
+        value
+      }
+
+      val v = MixedFreeS[MixedFreeS.Op]
+      import v._
+
+      val program = for {
+        a  <- z //3
+        bc <- (x |@| y).tupled.freeS //(1,2)
+        (b, c) = bc
+        d <- z //3
+      } yield a :: b :: c :: d :: Nil // List(3,1,2,3)
+
+    }
+
+    "allow non deterministic execution when interpreting to scala.concurrent.Future" in {
+      import freestyle.nondeterminism._
+      import freestyle.implicits._
+
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      val test = new NonDeterminismTestShared
+      import test._
+
+      implicit val interpreter = new MixedFreeS.Handler[Future] {
+        override def x: Future[Int] = Future(blocker(1, 1000L))
+        override def y: Future[Int] = Future(blocker(2, 0L))
+        override def z: Future[Int] = Future(blocker(3, 2000L))
+      }
+
+      Await.result(program.exec[Future], Duration.Inf) shouldBe List(3, 1, 2, 3)
+      buf.toArray shouldBe Array(3, 2, 1, 3)
+    }
+
+    "allow non deterministic execution when interpreting to monix.eval.Task" in {
+      import freestyle.nondeterminism._
+      import freestyle.implicits._
+
+      import monix.cats._
+      import monix.eval.Task.nondeterminism
+      import monix.execution.Scheduler.Implicits.global
+
+      val test = new NonDeterminismTestShared
+      import test._
+
+      implicit val interpreter = new MixedFreeS.Handler[Task] {
+        override def x: Task[Int] = Task(blocker(1, 1000L))
+        override def y: Task[Int] = Task(blocker(2, 0L))
+        override def z: Task[Int] = Task(blocker(3, 2000L))
+      }
+
+      Await.result(program.exec[Task].runAsync, Duration.Inf) shouldBe List(3, 1, 2, 3)
+      buf.toArray shouldBe Array(3, 2, 1, 3)
+    }
+
+    "allow deterministic programs with FreeS.Par nodes run deterministically" in {
+      import freestyle.nondeterminism._
+      import freestyle.implicits._
+
+      val test = new NonDeterminismTestShared
+      import test._
+
+      implicit val interpreter = new MixedFreeS.Handler[Option] {
+        override def x: Option[Int] = Option(blocker(1, 1000L))
+        override def y: Option[Int] = Option(blocker(2, 0L))
+        override def z: Option[Int] = Option(blocker(3, 2000L))
+      }
+
+      program.exec[Option] shouldBe Option(List(3, 1, 2, 3))
+      buf.toArray shouldBe Array(3, 1, 2, 3)
+    }
+
+    /**
+     * Similar example as the one found at
+     * http://typelevel.org/cats/datatypes/freeapplicative.html
+     */
+    "allow validation style algebras derived from FreeS.Par" in {
+      import cats.implicits._
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      import freestyle.nondeterminism._
+      import freestyle.implicits._
+
+      type ParValidator[A] = Kleisli[Future, String, A]
+
+      @free
+      trait Validation[F[_]] {
+        def minSize(n: Int): FreeS.Par[F, Boolean]
+        def hasNumber: FreeS.Par[F, Boolean]
+      }
+
+      implicit val interpreter = new Validation.Handler[ParValidator] {
+        override def minSize(n: Int): ParValidator[Boolean] =
+          Kleisli(s => Future(s.size >= n))
+        override def hasNumber: ParValidator[Boolean] =
+          Kleisli(s => Future(s.exists(c => "0123456789".contains(c))))
+      }
+
+      val validation = Validation[Validation.Op]
+      import validation._
+
+      val parValidation = (minSize(3) |@| hasNumber).map(_ :: _ :: Nil)
+      val validator     = parValidation.exec[ParValidator]
+
+      Await.result(validator.run("a"), Duration.Inf) shouldBe List(false, false)
+      Await.result(validator.run("abc"), Duration.Inf) shouldBe List(true, false)
+      Await.result(validator.run("abc1"), Duration.Inf) shouldBe List(true, true)
+      Await.result(validator.run("1a"), Duration.Inf) shouldBe List(false, true)
+    }
+  }
+
+}
