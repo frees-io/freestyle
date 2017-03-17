ec6616e3f870f1a5465b27ef77e545741f5b3c75
Alejandro Gómez <alejandro@dialelo.com>
2017-03-17 12:45:17 +0100

    Test that results are written in the responses

--- freestyle-play/src/test/scala/PlayTests.scala
+++ freestyle-play/src/test/scala/PlayTests.scala
@@ -15,17 +15,15 @@ import freestyle.http.play.implicits._
 
 import _root_.play.api.mvc._
 import _root_.play.api.http._
+import _root_.play.api.test._
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
 
 class PlayTests extends AsyncWordSpec with Matchers {
-  import app._
-  import cats.instances.future._
-
   implicit override def executionContext = ExecutionContext.Implicits.global
 
-  def program[F[_] : Noop]: FreeS[F, Result] = for {
-    x <- Noop[F].noop
-  } yield Results.Ok(x)
-
+  // play
 
   implicit def unitWr(implicit C: Codec): Writeable[Unit] = {
     Writeable(data => C.encode(data.toString))
@@ -33,19 +31,47 @@ class PlayTests extends AsyncWordSpec with Matchers {
 
   implicit val unitCT: ContentTypeOf[Unit] = new ContentTypeOf(Option("text/plain"))
 
+  // akka
+
+  implicit val actorSys: ActorSystem = ActorSystem("test")
+  implicit val materializer: Materializer = ActorMaterializer()
+
   "Play integration" should {
+    import cats.instances.future._
+
+    import algebras._
+    import handlers._
+
+    def program[F[_] : Noop]: FreeS[F, Result] = for {
+      x <- Noop[F].noop
+    } yield Results.Ok(x)
+
     "FreeSAction creates an action" in {
       FreeSAction { program[Noop.Op] }.isInstanceOf[Action[Result]] shouldBe true
+    }
+
+    "The action writes the result in the response" in {
+      import Helpers._
+
+      val action: EssentialAction = FreeSAction { program[Noop.Op] }
+      val request = FakeRequest()
+      val response: Future[Result] = Helpers.call(action, request)
+
+      status(response) shouldBe 200
+      contentAsString(response) shouldBe "()"
     }
   }
 }
 
-
-object app {
+object algebras {
   @free
   trait Noop[F[_]] {
     def noop: FreeS[F, Unit]
   }
+}
+
+object handlers {
+  import algebras._
 
   implicit def noopHandler[M[_]](
     implicit
