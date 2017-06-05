fc6f3535705d563fa3b0b52af35289fa4b614daa
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-06-05 14:02:03 +0200

    Publishes integrations project locally as well

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,8 @@ INTEGRATIONS_REPO="https://github.com/frees-io/freestyle-integrations.git"
 
 VERSION="$(grep -F -m 1 'version in ThisBuild :=' version.sbt)"; VERSION="${VERSION#*\"}"; VERSION="${VERSION%\"*}"
 
+echo "Checking projects $DOCS_REPO and $INTEGRATIONS_REPO for freestyle version $VERSION"
+
 set -x
 if [ $(git ls-remote $INTEGRATIONS_REPO | grep "refs/heads/$TRAVIS_BRANCH" | wc -l) = 1 ] ; then
     git clone --branch $TRAVIS_BRANCH --depth=1 $INTEGRATIONS_REPO
@@ -16,7 +18,7 @@ fi
 set +x
 
 sbt ++$TRAVIS_SCALA_VERSION publishLocal
-cd freestyle-integrations && sbt ++$TRAVIS_SCALA_VERSION -Dfrees.version=$VERSION "clean" "compile" "test"  && cd ..
+cd freestyle-integrations && sbt ++$TRAVIS_SCALA_VERSION -Dfrees.version=$VERSION "clean" "compile" "test" "publishLocal" && cd ..
 
 if [ "$TRAVIS_SCALA_VERSION" = "2.12.2" ]; then
   set -x
@@ -27,6 +29,5 @@ if [ "$TRAVIS_SCALA_VERSION" = "2.12.2" ]; then
   fi
   set +x
 
-  sbt ++$TRAVIS_SCALA_VERSION publishLocal
   cd freestyle-docs && sbt ++$TRAVIS_SCALA_VERSION -Dfrees.version=$VERSION "clean" "tut"  && cd ..
 fi
