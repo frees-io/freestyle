76cfde101ffad9c7fadfac5a9601a73858070b98
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-06-05 13:00:11 +0200

    Enables CI checks for integrations and docs projects

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+DOCS_REPO="https://github.com/frees-io/freestyle-docs.git"
+INTEGRATIONS_REPO="https://github.com/frees-io/freestyle-integrations.git"
+
+VERSION="$(grep -F -m 1 'version in ThisBuild :=' version.sbt)"; VERSION="${VERSION#*\"}"; VERSION="${VERSION%\"*}"
+
+set -x
+if [ $(git ls-remote $INTEGRATIONS_REPO | grep "refs/heads/$TRAVIS_BRANCH" | wc -l) = 1 ] ; then
+    git clone --branch $TRAVIS_BRANCH --depth=1 $INTEGRATIONS_REPO
+else
+    git clone --branch master --depth=1 $INTEGRATIONS_REPO
+fi
+set +x
+
+sbt ++$TRAVIS_SCALA_VERSION publishLocal
+cd freestyle-integrations && sbt ++$TRAVIS_SCALA_VERSION -Dfrees.version=$VERSION "clean" "compile" "test"  && cd ..
+
+if [ "$TRAVIS_SCALA_VERSION" = "2.12.2" ]; then
+  set -x
+  if [ $(git ls-remote $DOCS_REPO | grep "refs/heads/$TRAVIS_BRANCH" | wc -l) = 1 ] ; then
+      git clone --branch $TRAVIS_BRANCH --depth=1 $DOCS_REPO
+  else
+      git clone --branch master --depth=1 $DOCS_REPO
+  fi
+  set +x
+
+  sbt ++$TRAVIS_SCALA_VERSION publishLocal
+  cd freestyle-docs && sbt ++$TRAVIS_SCALA_VERSION -Dfrees.version=$VERSION "clean" "tut"  && cd ..
+fi
