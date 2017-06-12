625e20b574bcac48c05f3103195d723467163a10
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-06-13 01:01:38 +0200

    Settings moved to sbt-freestyle-plugin

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,9 +1,7 @@
 import freestyle.FreestylePlugin
 import sbt._
-import sbt.Keys._
 import sbtorgpolicies.OrgPoliciesPlugin.autoImport.orgScriptTaskListSetting
 import sbtorgpolicies.runnable.syntax._
-import scoverage.ScoverageKeys.coverageExcludedFiles
 
 object ProjectPlugin extends AutoPlugin {
 
@@ -11,27 +9,10 @@ object ProjectPlugin extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  object autoImport {
-
-    def toCompileTestList(sequence: Seq[ProjectReference]): List[String] = sequence.toList.map {
-      p =>
-        val project: String = p.asInstanceOf[LocalProject].project
-        s"$project/test"
-    }
-
-  }
+  object autoImport
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    coverageExcludedFiles in Global := ".*<macro>",
-    orgScriptTaskListSetting := List("validate".asRunnableItemFull),
-    packageDoc in Compile := {
-      val sourceFile = (baseDirectory in LocalRootProject).value / "README.md"
-      val targetFile = crossTarget.value / s"${name.value}-javadoc.jar"
-
-      IO.copy(Seq(sourceFile -> targetFile), overwrite = true).toSeq
-
-      targetFile
-    }
+    orgScriptTaskListSetting := List("validate".asRunnableItemFull)
   )
 
 }
