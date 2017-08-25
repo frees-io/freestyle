24456d92b30209ff9cb1fa5f309601accfc2ca99
Peter Neyens <peter.neyens@gmail.com>
2017-08-25 13:16:19 +0100

    Rename modules to frees-xxx (#396)
    Create modules folder and move all modules to this folder.

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,5 +1,7 @@
 import freestyle.FreestylePlugin
+import org.scalajs.sbtplugin.cross.{CrossProject, CrossType}
 import sbt._
+import sbt.Keys.moduleName
 import sbtorgpolicies.OrgPoliciesPlugin.autoImport._
 import sbtorgpolicies.runnable.syntax._
 
@@ -9,7 +11,20 @@ object ProjectPlugin extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  object autoImport
+  object autoImport {
+
+    def module(modName: String, hideFolder: Boolean = false, full: Boolean = true): CrossProject =
+      CrossProject(
+        modName, 
+        file(s"""modules/${if (hideFolder) "." else ""}$modName"""), 
+        if (full) CrossType.Full else CrossType.Pure
+      ).settings(moduleName := s"frees-$modName")
+
+    def jvmModule(modName: String): Project =
+      Project(modName, file(s"""modules/$modName"""))
+        .settings(moduleName := s"frees-$modName")
+
+  }
 
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
