08cd997a54ce3c617d4e2c6f48445ec8b43ed0d8
Raúl Raja Martínez <raulraja@gmail.com>
2017-11-19 21:38:47 +0100

    Make Algebras and Modules concrete with `.instance` (#447)
    * Enables `A.instance` as alternative to `A[A.Op]`
    
    * Enables `A.instance` in single algebras

--- modules/core/shared/src/main/scala/freestyle/internal/free.scala
+++ modules/core/shared/src/main/scala/freestyle/internal/free.scala
@@ -153,7 +153,7 @@ private[internal] case class Algebra(
     """
   }
 
-  def lifterStats: (Class, Defn.Def, Defn.Def) = {
+  def lifterStats: (Class, Defn.Def, Defn.Def, Defn.Def) = {
     val gg: Type.Name               = Type.fresh("LL$") // LL is the target of the Lifter's Injection
     val injTParams: Seq[Type.Param] = tyParamK(gg) +: cleanedTParams
     val injTArgs: Seq[Type]         = gg +: cleanedTParams.map(toType)
@@ -180,7 +180,10 @@ private[internal] case class Algebra(
       q"def apply[..$injTParams](implicit $ev: $name[..$injTArgs]): $name[..$injTArgs] = $ev"
     }
 
-    (toClass, toDef, applyDef)
+    val applyDefConcreteOp: Defn.Def =
+      q"def instance(implicit ev: $name[$OP]): $name[$OP] = ev"
+
+    (toClass, toDef, applyDef, applyDefConcreteOp)
   }
 
   def mkCompanion: Object = {
@@ -190,7 +193,7 @@ private[internal] case class Algebra(
     }
     val opTypes                    = q"type OpTypes = _root_.iota.TConsK[$OP, _root_.iota.TNilK]"
     val adt: Seq[Stat]             = opTrait +: requests.map(_.reqClass(OP, cleanedTParams, indexName))
-    val (toClass, toDef, applyDef) = lifterStats
+    val (toClass, toDef, applyDef, applyDefConcrete) = lifterStats
     val prot                       = q"""@_root_.java.lang.SuppressWarnings(_root_.scala.Array(
                                            "org.wartremover.warts.Any",
                                            "org.wartremover.warts.AsInstanceOf",
@@ -201,7 +204,7 @@ private[internal] case class Algebra(
     prot.copy(
       name = Term.Name(name.value),
       templ = prot.templ.copy(
-        stats = Some(adt ++ Seq(opTypes, handlerTrait, toClass, toDef, applyDef))
+        stats = Some(adt ++ Seq(opTypes, handlerTrait, toClass, toDef, applyDef, applyDefConcrete))
       ))
   }
 
