2ba148ed1406c315544f0e30d425a4d3c2b62e28
Diego Esteban Alonso Blas <diesalbla@gmail.com>
2018-01-02 14:14:30 +0100

    Update License to 2018 (#489)

--- modules/core/shared/src/main/scala/freestyle/free/internal/free.scala
+++ modules/core/shared/src/main/scala/freestyle/free/internal/free.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 47 Degrees, LLC. <http://www.47deg.com>
+ * Copyright 2017-2018 47 Degrees, LLC. <http://www.47deg.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
