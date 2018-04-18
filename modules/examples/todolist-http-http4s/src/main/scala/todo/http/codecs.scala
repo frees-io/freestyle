cf730407c2b4eef5a23efb7ae94fcc9475404447
Jesús Martínez <JesusMtnez@users.noreply.github.com>
2018-04-18 09:13:04 +0200

    TodoList example using http4s (#545)
    * Add basic module setup
    * WIP Basic example with ping/pong api
    * Fix TodoListApp
    * Make TodoListApp run forever
    * Add logs when starting application
    * WIP
    * Add logs in GenericApi
    * Add TagApi
    * Change GenericApi to homogenize
    * Add TodoItemApi
    * Add TodoListApi
    * Add API to unify all apis
    * Fix reset endpoint in TagApi
    * Add AppApi
    * Sort imports in TodoListApp
    * Use wildcard when importing multiple classes from package
    * Support env vars fro configuration
    * Move json {en,de}coders to it own object
    * Use apply product when it's possible
    * Sort imports in all the proyect
    * Improve {en,de}coders signature

---
+++ modules/examples/todolist-http-http4s/src/main/scala/todo/http/codecs.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017-2018 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package examples.todolist
+package http
+
+import cats.Applicative
+import cats.effect.Sync
+import io.circe.generic.auto._
+import org.http4s._
+import org.http4s.circe._
+
+object codecs {
+
+  implicit def tagEncoder[F[_]: Applicative]: EntityEncoder[F, Tag] = jsonEncoderOf[F, Tag]
+  implicit def tagDecoder[F[_]: Sync]: EntityDecoder[F, Tag]        = jsonOf[F, Tag]
+
+  implicit def todoItemEncoder[F[_]: Applicative]: EntityEncoder[F, TodoItem] =
+    jsonEncoderOf[F, TodoItem]
+  implicit def todoItemDecoder[F[_]: Sync]: EntityDecoder[F, TodoItem] = jsonOf[F, TodoItem]
+
+  implicit def todoListEncoder[F[_]: Applicative]: EntityEncoder[F, TodoList] =
+    jsonEncoderOf[F, TodoList]
+  implicit def todoListDecoder[F[_]: Sync]: EntityDecoder[F, TodoList] = jsonOf[F, TodoList]
+
+  implicit def todoFormEncoder[F[_]: Applicative]: EntityEncoder[F, TodoForm] =
+    jsonEncoderOf[F, TodoForm]
+  implicit def todoFormDecoder[F[_]: Sync]: EntityDecoder[F, TodoForm] =
+    jsonOf[F, TodoForm]
+}
